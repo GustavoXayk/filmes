7a8516186ee82f564b00413e46d70d45ada36043
Narutao <luizpaiva43@hotmail.com>
2023-03-23 09:35:34 -0300

    Backend projeto XaykFilmes

---
+++ src/main/java/dev/gusxayk/movies/MovieController.java
@@ -0,0 +1,27 @@
+package dev.gusxayk.movies;
+
+import java.util.List;
+import java.util.Optional;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    @CrossOrigin(origins="http://localhost:3000")
+    public ResponseEntity<List<Movie>> getAllMovies(){
+       return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    @CrossOrigin(origins="http://localhost:3000")
+    public  ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/dev/gusxayk/movies/Review.java
@@ -0,0 +1,21 @@
+package dev.gusxayk.movies;
+
+import org.springframework.data.annotation.Id;
+import org.bson.types.ObjectId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
+}
